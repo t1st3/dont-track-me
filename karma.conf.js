fa4891cd3e6ded0499707b8db5ee2eb8eaa0e244
t1st3 <contact@t1st3.com>
2017-11-18 17:52:31 +0100

    chore: add code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,27 +1,32 @@
 module.exports = function(config) {
-  config.set({
-    basePath: '',
-    frameworks: ['mocha', 'chai'],
-    files: [
-      'src/dont-track-me.js',
-      'test/dont-track-me.test.js'
-    ],
-    exclude: [
-    ],
-    client: {
-      mocha: {
-        reporter: 'html',
-      },
-    },
-    preprocessors: {
-    },
-    reporters: ['dots'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['Firefox', 'Chromium'],
-    singleRun: true,
-    concurrency: Infinity,
-  });
+	config.set({
+		basePath: '',
+		frameworks: ['mocha', 'chai'],
+		files: [
+			'src/dont-track-me.js',
+			'test/dont-track-me.test.js'
+		],
+		exclude: [
+		],
+		client: {
+			mocha: {
+				reporter: 'html',
+			},
+		},
+		preprocessors: {
+			'src/dont-track-me.js': ['coverage']
+		},
+		reporters: ['progress', 'coverage'],
+		coverageReporter: {
+			type : 'html',
+			dir : 'coverage/'
+		},
+		port: 9876,
+		colors: true,
+		logLevel: config.LOG_INFO,
+		autoWatch: false,
+		browsers: ['Firefox', 'Chromium'],
+		singleRun: true,
+		concurrency: Infinity,
+	});
 };
